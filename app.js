7928bc80b91ab54acb9fb98bd90d1ca4b127e82e
SamixYasuke <samueladekolu4@gmail.com>
2024-10-05 23:20:56 +0100

    update: update servers url in app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import Stripe from 'stripe';
 import dotenv from 'dotenv';
 import swaggerJSDoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express';
-import cors from 'cors';
 
 dotenv.config();
 
@@ -31,7 +30,6 @@ const swaggerOptions = {
 const swaggerDocs = swaggerJSDoc(swaggerOptions);
 
 const app = express();
-app.use(cors()); 
 app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
